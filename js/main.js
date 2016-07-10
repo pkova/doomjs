332106656253380b2db8400cfb5a53331b2d2f2e
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-07-10 12:58:22 -0700

    Add createPlayer

--- js/main.js
+++ js/main.js
@@ -15,18 +15,18 @@ camera.position.z = 5;
 camera.position.y = 2;
 
 var sampleMap = [
-  [1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 0, 0, 1, 0, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
 ];
 
 var createMap = function(matrix) {
-  matrix.map(function(arr, yIdx) {
+  window.arr = matrix.map(function(arr, yIdx) {
     return arr.map(function(coord, xIdx) {
       if (coord === 1) {
-        var geometry = new THREE.BoxGeometry(10, 3, 10);
+        var geometry = new THREE.BoxGeometry(10, 10, 10);
         var material = new THREE.MeshNormalMaterial();
         var segment = new THREE.Mesh(geometry, material);
         segment.position.set(xIdx*10, 0, yIdx*10);
@@ -37,11 +37,47 @@ var createMap = function(matrix) {
   });
 };
 
+var createPlayer = function() {
+  var geometry = new THREE.BoxGeometry(2, 2, 5);
+  var material = new THREE.MeshNormalMaterial();
+  var playerCube = new THREE.Mesh(geometry, material);
+  return playerCube;
+};
+
+var checkCollision = function() {
+
+  cameraBBox.update();
+  var walls = scene.children.slice();
+  walls.pop();
+  walls.pop();
+  for (var i = 0; i < walls.length; i++) {
+    var mesh = walls[i];
+    var meshBBox = new THREE.BoundingBoxHelper(mesh);
+    meshBBox.update();
+    if (cameraBBox.box.intersectsBox(meshBBox.box)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var player = createPlayer();
 createMap(sampleMap);
+scene.add(camera);
+camera.add(player);
+
+var cameraBBox = new THREE.BoundingBoxHelper(camera);
+var axisHelper = new THREE.AxisHelper( 5 );
+scene.add( axisHelper );
 
 function render() {
-	requestAnimationFrame( render );
-	renderer.render( scene, camera );
-  controls.update(1);
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
+  if (checkCollision()) {
+    console.log('collision');
+  } else {
+    console.log('no collision');
+    controls.update(1);
+  }
 }
 render();
