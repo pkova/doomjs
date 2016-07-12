50b70f64363bbe336eb6dd38dd80e22dabcdf6bc
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-07-11 22:34:34 -0700

    Add environment variable for port

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
